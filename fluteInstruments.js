bc4b10087ada9164c445b01fe9675c0ade7fecc6
thelittlecitizen16 <thelittlecitizen16@gmail.com>
2020-09-14 10:46:32 +0300

    take instruments from json

--- fluteInstruments.js
+++ fluteInstruments.js
@@ -17,5 +17,45 @@ var fluteInstrument = [{
     name: "harmonica",
     price: "35₪",
     typeID: 1
+  },
+  {
+    description: "beatiful flute",
+    id: 3,  
+    imagePath: "./images/flute.gif",
+    instrumentOrders: null,
+    instrumentType: {id: 1, name: "נשיפה"},
+    name: "flute",
+    price: "35₪",
+    typeID: 1
+  }, 
+  {
+    description: "little trombone",
+    id: 4,  
+    imagePath: "./images/trombone.jpg",
+    instrumentOrders: null,
+    instrumentType: {id: 1, name: "נשיפה"},
+    name: "trombone",
+    price: "2400₪",
+    typeID: 1
+  },
+  {
+    description: "little Clarinet",
+    id: 5,  
+    imagePath: "./images/Clarinet.jpg",
+    instrumentOrders: null,
+    instrumentType: {id: 1, name: "נשיפה"},
+    name: "Clarinet",
+    price: "1400₪",
+    typeID: 1
+  },
+  {
+    description: "little violin",
+    id: 6,  
+    imagePath: "./images/violin.jpg",
+    instrumentOrders: null,
+    instrumentType: {id: 1, name: "נשיפה"},
+    name: "violin",
+    price: "2300₪",
+    typeID: 1
   }]
   
--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 
-function A(){
+function AddAllMusicalInstruments(){
     fluteInstrument.forEach(element => {
-        AddAllMusicalInstruments(element.imagePath,element.title,element.description,element.price);
+        AddMusicalInstrument(element.imagePath,element.name,element.description,element.price);
     });
 }
 
-function AddAllMusicalInstruments(imgSrc, title,description,price) {
+function AddMusicalInstrument(imgSrc, title,description,price) {
     console.table(fluteInstrument);
+
     var html = `<div class="col-sm-3 border d-flex justify-content-center text-center MusicalInstruments"><div class="MusicalInstruments">
     <img  class = "MusicalInstrumentsImg" class="img-fluid" src=${imgSrc} alt="Clarinet" id="Clarinet">
     <p class="title">${title}</p>
@@ -17,8 +18,8 @@ function AddAllMusicalInstruments(imgSrc, title,description,price) {
     </button>
     </div>
     </div>`
+
     document.getElementById('allMusicalInstruments').innerHTML += html;
 }
 
-//AddAllMusicalInstruments("./images/Clarinet.jpg","try","try2","415");
-A();
+AddAllMusicalInstruments();
