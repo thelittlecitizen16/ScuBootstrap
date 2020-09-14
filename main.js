b7969960523b1df472dbee9051c745df9eb59b94
thelittlecitizen16 <thelittlecitizen16@gmail.com>
2020-09-14 11:57:57 +0300

    add functions

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 
-function AddAllMusicalInstruments(){
+let currentMusicalInstruments;
+function AddAllMusicalInstruments(fluteInstrument){
+    document.getElementById('allMusicalInstruments').innerHTML = "";
     fluteInstrument.forEach(element => {
         AddMusicalInstrument(element.imagePath,element.name,element.description,element.price);
     });
 }
 
 function AddMusicalInstrument(imgSrc, title,description,price) {
-    //console.table(fluteInstrument);
-
     var html = `<div class="col-sm-3 border d-flex justify-content-center text-center MusicalInstruments"><div class="MusicalInstruments">
     <img  class = "MusicalInstrumentsImg" class="img-fluid" src=${imgSrc} alt="Clarinet" id="Clarinet">
     <p class="title">${title}</p>
@@ -20,10 +20,11 @@ function AddMusicalInstrument(imgSrc, title,description,price) {
     </div>`
 
     document.getElementById('allMusicalInstruments').innerHTML += html;
+    currentMusicalInstruments=fluteInstrument;
 }
 
 
-AddAllMusicalInstruments();
+AddAllMusicalInstruments(fluteInstrument);
 
 function GetInstrumentsByOption(optionValue) {
     if (optionValue=="all")
@@ -38,20 +39,26 @@ function GetInstrumentsByOption(optionValue) {
 function ChangeByOption() {
     let optionValue = document.getElementById("roledDopdown").value;
     let fluteInstrumentByType= GetInstrumentsByOption(optionValue);
+    currentMusicalInstruments=fluteInstrumentByType;
 
-    document.getElementById('allMusicalInstruments').innerHTML = "";
-    fluteInstrumentByType.forEach(element => {
-        AddMusicalInstrument(element.imagePath,element.name,element.description,element.price);
-    }); 
+    AddAllMusicalInstruments(currentMusicalInstruments);
 }
 
 function search() {
     let data = document.getElementById("search").value;
+
+    fluteInstrumentByType= currentMusicalInstruments.filter(i=>i.name.includes(data));
+    currentMusicalInstruments=fluteInstrumentByType;
+    AddAllMusicalInstruments(fluteInstrumentByType); 
+};
+
+
+function changeByPrice() {
+    let data = document.getElementById("search").value;
     let optionValue = document.getElementById("roledDopdown").value;
     let fluteInstrumentByType= GetInstrumentsByOption(optionValue);
+
     fluteInstrumentByType= fluteInstrumentByType.filter(i=>i.name.includes(data));
     document.getElementById('allMusicalInstruments').innerHTML = "";
-    fluteInstrumentByType.forEach(element => {
-        AddMusicalInstrument(element.imagePath,element.name,element.description,element.price);
-    }); 
+    AddAllMusicalInstruments(fluteInstrumentByType); 
 };
